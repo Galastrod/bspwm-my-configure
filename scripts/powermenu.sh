0c291e5cc525d081bc63992c0d981cb7696b2441
Albert <albertmagnus829@gmail.com>
2025-06-29 13:26:06 +0000

    first commit

--- scripts/powermenu.sh
+++ scripts/powermenu.sh
@@ -4,10 +4,11 @@ rofi_cmd() {
   rofi -dmenu -theme ~/.config/rofi/powermenu.rasi
 }
 
-chosen=$(printf "\uf204  Poweroff\n\uF2f9  Reboot\n\uF2f5  Logout" | rofi_cmd)
+chosen=$(printf "\n\n" | rofi_cmd)
 
 case "$chosen" in
-  "?  poweroff") poweroff ;;
-  "?  reboot") reboot ;;
-  "?  logout") bspc quit ;;
+  "?	poweroff") poweroff ;;
+  "?	reboot") reboot ;;
+  "?	logout") bspc quit ;;
 esac
+
